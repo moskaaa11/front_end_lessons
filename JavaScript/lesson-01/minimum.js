b0c0b33a71fba9f068f9252b86cfae16d4265f11
moskaaa11 <moskaaa11@gamil.com>
2022-06-27 10:58:48 +0300

    commit

---
+++ JavaScript/lesson-01/minimum.js
@@ -0,0 +1,20 @@
+let userName1;
+
+let userNick1;
+
+let userName2;
+
+let userNick2;
+
+
+/* let let =5 */
+
+/* let class =5 */
+
+/* let return =5 */
+
+/* let function =5 */
+
+/* let 1a =5 */
+
+/* let my-name =5 */
---
+++ JavaScript/lesson-01/normal.js
@@ -0,0 +1,16 @@
+let userName = prompt('Як тебе звати?');
+
+alert(`Привіт, ${userName}!`); 
+
+let x = 2022;
+
+let age = prompt ('Який рік твого народження?');
+
+const userAge = x- age;
+
+alert(`Тобі ${userAge} років!`);
+
+let square = prompt ('Назви довжину квадрату');
+
+alert(`Його периметр дорівнює ${square*square}`);
+
