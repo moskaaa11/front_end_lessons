6ebbd37b9d3908336a60db8ed5d5bee2de03b19f
moskaaa11 <moskaaa11@gamil.com>
2022-06-27 11:13:38 +0300

    commit

---
+++ JavaScript/lesson-01/maximum.js
@@ -0,0 +1,21 @@
+//first 
+let radius= prompt ('Назви периметр кола');
+
+let pi = 3.14;
+
+alert (`Його площа дорівнює ${pi* (radius**2) }`); 
+//second
+
+let dist = prompt ('Назви відстань між містами');
+
+let hour = prompt ('За скільки годин ти хочеш туди дійти?');
+
+alert (`Тобі потрібно рухатись зі швидкістю ${dist/hour} км/год`);
+
+//third
+
+let dollar = prompt ('Веди кількість долларів');
+
+const euro = 1.1;
+
+alert (`Ти отримаєш ${dollar*euro} євро`);
