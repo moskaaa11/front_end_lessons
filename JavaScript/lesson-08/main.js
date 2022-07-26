abe8ac020eafdbd14cb9d7a1661cd9201eecae42
moskaaa11 <moskaaa11@gmail.com>
2022-07-26 23:05:29 +0300

    commit

--- JavaScript/lesson-08/main.js
+++ JavaScript/lesson-08/main.js
@@ -19,4 +19,37 @@ let circle = new Circle;
 circle.square(10);
 circle.line(143);
 
+//first end
 
+class Marker {
+    constructor( ) {
+        this.color = 'red',
+        this.fill = 100,
+        this.text = 'new'
+    }
+        
+    write (text) {
+        this.text= text;
+     
+    }
+    
+
+    // set fill (value) {
+    //     if (value < 0) {
+    //       value = 0;
+    //     } else if (value > 100) {
+    //         value = 100;
+    //     }
+    //     this.fill = value;
+    //   }
+    
+    //   get fill() {
+    //     return this.fill;
+    //   }
+
+    
+}
+
+let words = new Marker;
+
+words.write('text')
---
+++ JavaScript/lesson-09/main.js
@@ -0,0 +1,78 @@
+let out = document.querySelector('.out');
+let btn = document.querySelector('.btn');
+var playList = [
+    {
+        author: "LED ZEPPELIN",
+        song:"STAIRWAY TO HEAVEN"
+    },
+    {
+        author: "QUEEN",
+        song:"BOHEMIAN RHAPSODY"
+    },
+    
+    {
+        author: "LYNYRD SKYNYRD",
+        song:"FREE BIRD"
+    },
+    
+    {
+        author: "DEEP PURPLE",
+        song:"SMOKE ON THE WATER"
+    },
+    
+    {
+        author: "JIMI HENDRIX",
+        song:"ALL ALONG THE WATCHTOWER"
+    },
+    
+    {
+        author: "AC/DC",
+        song:"BACK IN BLACK"
+    },
+    
+    {
+        author: "QUEEN",
+        song:"WE WILL ROCK YOU"
+    },
+    
+    {
+        author: "METALLICA",
+        song:"ENTER SANDMAN"
+    }
+    ];
+let res = '';
+
+function song() {
+
+    for (i in playList) {
+        res += i;
+        for (key in playList[i]) {
+          res += " " + playList[i][key] + '<br>';
+        }
+      }
+    out.innerHTML=res;
+}
+
+btn.onclick=song;
+
+//first end
+
+let out1= document.querySelector('.out-1');
+let btn1 = document.querySelector ('.btn-1');
+let btn2 = document.querySelector ('.btn-2');
+
+btn1.onclick = () => {
+    out1.style.display = 'block';
+};
+btn2.onclick = () => {
+    out1.style.display = 'none';
+}
+
+// second end
+
+let btn3 = document.querySelector ('btn-3');
+let box = document.querySelector ('container__box')
+
+btn3.onclick = () => {
+    box.style.display = 'none';
+}
